59ea8d8bac1c4a292746342355ac8c16773c3b47
OKnischay <nischaybasi015@gmail.com>
2025-03-07 16:37:44 +0545

    feat: implement dashboard pages

--- client/src/app/(dashboard)/dashboard/leave/page.tsx
+++ client/src/app/(dashboard)/dashboard/leave/page.tsx
@@ -1,12 +1,26 @@
 import LeaveHandling from '@/components/dashboard/leave/LeaveHandling'
+import { Button } from '@/components/ui/button'
+import { PlusCircle } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const Leave = () => {
-  return (
-    <div>
-        <LeaveHandling/>
-    </div>
-  )
+    return (
+        <div className='w-full px-6 flex flex-col items-start justify-center'>
+{/* 
+            <h2 className='text-3xl font-semibold'>Leave</h2>
+            <div className='w-full flex justify-between items-center'>
+                <div></div>
+                <Link href={'/'}>
+                    <Button className=''>
+                        <PlusCircle />
+                        <span>Add leave</span>
+                    </Button>
+                </Link>
+            </div> */}
+            <LeaveHandling />
+        </div>
+    )
 }
 
 export default Leave
