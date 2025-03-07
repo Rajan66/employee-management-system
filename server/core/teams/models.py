0df8ffbdeceb13a09354a988f446564c26e9676b
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-07 13:26:38 +0545

    chore: remove departments

--- server/core/teams/models.py
+++ server/core/teams/models.py
@@ -1,20 +1,21 @@
+from core.base.models import BaseModel
 from django.db import models
 from employees.models import Employee
-from departments.models import Department
-from core.base.models import BaseModel
 
 
 class Team(BaseModel):
     name = models.CharField(max_length=255, unique=True)
-    department = models.ForeignKey(
-        Department, on_delete=models.CASCADE, related_name="teams"
-    )
+    # department = models.ForeignKey(
+    #     Department, on_delete=models.CASCADE, related_name="teams"
+    # )
     lead = models.ForeignKey(
-        Employee, on_delete=models.SET_NULL, null=True, blank=True, related_name="leading_teams"
+        Employee,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="leading_teams",
     )
-    members = models.ManyToManyField(
-        Employee, related_name="teams", blank=True
-    )
+    members = models.ManyToManyField(Employee, related_name="teams", blank=True)
 
     def __str__(self):
         return f"{self.name} ({self.department.name})"
