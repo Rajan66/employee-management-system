da7bb47f604807cba841c02cb9c44cb9ec4e517d
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-07 19:59:16 +0545

    chore: set attendance and leave to admin

--- server/core/employees/admin.py
+++ server/core/employees/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from employees.models import Employee, Salary, SalaryPayment
+
+from employees.models import Attendance, Employee, Leave, Salary, SalaryPayment
 
 # Register your models here.
 admin.site.register(Employee)
 admin.site.register(Salary)
 admin.site.register(SalaryPayment)
+admin.site.register(Attendance)
+admin.site.register(Leave)
